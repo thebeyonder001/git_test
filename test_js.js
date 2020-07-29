d09b8fa43ada669d8b9a4473c2b107c9a4c47895
thebeyonder001 <bjain.1503@gmail.com>
2020-07-29 22:11:11 +0530

    fourth commit

--- test_js.js
+++ test_js.js
@@ -6,29 +6,38 @@ for(var i=0;i<act.length;i++){
     });
 }
 
-// document.addEventListener("keydown", function (event){
-//     makeSound(event.key);
-//   });
+document.addEventListener("keydown", function (event){
+    makeSound(event.key);
+  });
 
 function makeSound(key){
+var myAudio_w = document.getElementById('w');
+var myAudio_s = document.getElementById('s');
+var myAudio_a = document.getElementById('a');
+var myAudio_d = document.getElementById('d');
+
   switch (key){
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
+    //   var tom1 = new Audio("sounds/w.mid");
+    //   tom1.play();
+    myAudio_w.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
+    //   var tom2 = new Audio("sounds/a.mid");
+    //   tom2.play();
+    myAudio_s.play();
       break;
 
     case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
+    //   var tom3 = new Audio("sounds/s.mid");
+    //   tom3.play();
+    myAudio_a.play();
       break;
     case "d":
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
+    //   var tom4 = new Audio("sounds/d.mid");
+    //   tom4.play();
+    myAudio_d.play();
       break;
 
     default: console.log(buttonInnerHTML);
